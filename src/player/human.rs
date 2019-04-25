aa5fcc6d52d6324bcb9d9e79e329d6ad4b64bc7d
Logan Saso <logansaso@gmail.com>
2019-04-24 23:56:16 -0700

    Somewhat unified logic

--- src/player/human.rs
+++ src/player/human.rs
@@ -4,6 +4,44 @@ use std::io::prelude::*;
 
 pub struct HumanPlayer(pub char);
 
+impl HumanPlayer {
+    fn move_helper(
+        &self,
+        prompt: &'static str,
+        upper_bound: usize,
+        game: &Othello,
+    ) -> Option<usize> {
+        let mut val = "".to_string();
+
+        print!("{}", prompt);
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut val).unwrap();
+        let val = val.trim();
+
+        if val == "?" {
+            println!("Moves:");
+            for (row, col) in game.successors(self.get_symbol()) {
+                println!("Row: {}, Col: {}", row, col);
+            }
+            return None;
+        }
+
+        match val.parse::<usize>() {
+            Ok(val) if val < upper_bound => Some(val),
+            Ok(_) => {
+                eprintln!("Your entry is out of range.");
+                None
+            }
+            _ => {
+                eprintln!("You must enter positive numerical numerical values.");
+                None
+            }
+        }
+    }
+}
+
+const LOOP_ERR: &str = "Tried to unwrap after a loop that prevents None values.";
+
 impl Player for HumanPlayer {
     fn get_symbol(&self) -> char {
         self.0
@@ -11,53 +49,29 @@ impl Player for HumanPlayer {
 
     fn get_move(&self, game: &Othello) -> (usize, usize) {
         let board = game.board();
+        let mut row = None;
+        let mut col = None;
+
         loop {
-            let mut row = "".to_string();
-            let mut col = "".to_string();
-            print!("Enter row: ");
-            std::io::stdout().flush().unwrap();
-            std::io::stdin().read_line(&mut row).unwrap();
-
-            if row.trim() == "?" {
-                println!("Moves:");
-                for (row, col) in game.successors(self.get_symbol()) {
-                    println!("Row: {}, Col: {}", row, col);
+            if row.is_none() {
+                row = self.move_helper("Enter row (or ?): ", board.rows(), game);
+                if row.is_none() {
+                    continue;
                 }
-                continue;
             }
-
-            print!("Enter col: ");
-            std::io::stdout().flush().unwrap();
-            std::io::stdin().read_line(&mut col).unwrap();
-
-            let rown = match row.trim().parse::<usize>() {
-                Ok(rown) if rown < board.rows() => rown,
-                Ok(_) => {
-                    eprintln!("Your entry is out of range.");
+            if col.is_none() {
+                col = self.move_helper("Enter col (or ?): ", board.cols(), game);
+                if col.is_none() {
                     continue;
                 }
-                _ => {
-                    eprintln!("You must enter positive numerical numerical values.");
-                    continue;
-                }
-            };
-
-            let coln = match col.trim().parse::<usize>() {
-                Ok(coln) if coln < board.cols() => coln,
-                Ok(_) => {
-                    eprintln!("Your entry is out of range.");
-                    continue;
-                }
-                _ => {
-                    eprintln!("You must enter positive numerical numerical values.");
-                    continue;
-                }
-            };
-
-            if board.is_cell_empty(rown, coln) {
-                return (rown, coln);
+            }
+            // These expects are safe due to the conditions above. Changing them can result in unexpected behavior
+            if board.is_cell_empty(row.expect(LOOP_ERR), col.expect(LOOP_ERR)) {
+                return (row.expect(LOOP_ERR), col.expect(LOOP_ERR));
             } else {
                 eprintln!("That cell is occupied");
+                row = None;
+                col = None;
             }
         }
     }
